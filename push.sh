f1b8603d83134948e91bdabeda56c2262718a931
Daniel Fuchs <dfuchs@synyx.de>
2018-11-24 10:13:34 +0100

    location fix

---
+++ push.sh
@@ -0,0 +1,13 @@
+cd ../skill-$1
+pwd
+git add .
+git commit -m "$2"
+git push origin master 
+cd ../mycroft-skills
+git pull
+git submodule update --remote skill-$1
+git add skill-$1
+git commit -m "update $1"
+git push origin 18.08
+cd -
+ssh mycorf@10.0.20.129 "./mycroft-core/bin/mycroft-msm -u https://github.com/HackYourOffice/mycroft-skills update skill-$1"
